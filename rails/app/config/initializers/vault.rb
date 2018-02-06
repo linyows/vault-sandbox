96e3837e713069ef156475283012c9f9b5e05f58
linyows <linyows@gmail.com>
2018-02-06 23:07:43 +0900

    skip ssl verify on rails

--- rails/app/config/initializers/vault.rb
+++ rails/app/config/initializers/vault.rb
@@ -1,23 +1,9 @@
-require "vault/rails"
+require 'vault/rails'
 
-Vault::Rails.configure do |vault|
-  # Use Vault in transit mode for encrypting and decrypting data. If
-  # disabled, vault-rails will encrypt data in-memory using a similar
-  # algorithm to Vault. The in-memory store uses a predictable encryption
-  # which is great for development and test, but should _never_ be used in
-  # production.
-  vault.enabled = Rails.env.production?
-
-  # The name of the application. All encrypted keys in Vault will be
-  # prefixed with this application name. If you change the name of the
-  # application, you will need to migrate the encrypted data to the new
-  # key namespace.
-  vault.application = "my_rails"
-
-  # The address of the Vault server. Default: ENV["VAULT_ADDR"].
-  vault.address = "https://localhost:8200"
-
-  # The token to communicate with the Vault server.
-  # Default: ENV["VAULT_TOKEN"].
-  vault.token = "abcd1234"
+Vault::Rails.configure do |c|
+  c.enabled = true
+  c.application = 'rails'
+  c.address = 'https://vault:8200'
+  c.token = 'abcd-1234'
+  c.ssl_verify = false
 end
